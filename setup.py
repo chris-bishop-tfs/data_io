78584df41c48bbf279eae2219ed45d268fa84407
Chris Bishop <chris.bishop@thermofisher.com>
2021-03-10 12:08:17 -0800

    adding setup file

---
+++ setup.py
@@ -0,0 +1,19 @@
+"""
+Basic setup file for connections library
+"""
+
+from setuptools import setup
+
+setup(
+    name='data_io',
+    # Hard code for now
+    version='0.0.1',
+    description='Standardize data IO at Thermo Fisher',
+    author='Christopher Bishop',
+    author_email='chris.bishop@thermofisher.com',,
+    # Note that this is a string of words separated by whitespace, not a list.
+    keywords='jupyter mapreduce nteract pipeline notebook',
+    url='https://github.com/chris-bishop-tfs/data_io',
+    packages=['data_io']
+    # XXX Add requirements
+)
