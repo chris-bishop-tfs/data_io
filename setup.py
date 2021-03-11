dce7592f68d57f9ff34c911821f4b8a9b25987d8
Chris Bishop <chris.bishop@thermofisher.com>
2021-03-10 17:29:04 -0800

    holy. poop. please.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 setup(
     name='data_io',
     # Hard code for now
-    version='0.0.6',
+    version='0.0.7',
     description='Standardize data IO at Thermo Fisher',
     author='Christopher Bishop',
     author_email='chris.bishop@thermofisher.com',
