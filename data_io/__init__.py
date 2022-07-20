6ab721c60900d9ef356948c88eac2cab8084e6ec
Katz <sidney.katz@thermofisher.com>
2022-07-20 16:03:54 -0700

    new test

--- data_io/__init__.py
+++ data_io/__init__.py
@@ -1,10 +1,12 @@
 from . import location
 from . import connection
 from . import data_source
+from . import get_credentials
 # from . import test_connection
 
 # Export (relevant) builders to expose API
 from .location import build_location
 from .connection import build_connection
 from .data_source import build_data_source
+from .get_credentials import GetConnection
 # from .test.test_connection import connection_test
---
+++ data_io/get_credentials.py
@@ -0,0 +1,61 @@
+from runtime.nutterfixture import NutterFixture, tag
+from data_io import build_connection
+import io
+from configparser import RawConfigParser, ConfigParser
+from urlpath import URL
+from pyspark.sql import SparkSession
+from pyspark.dbutils import DBUtils
+
+''' 
+we have now written entire class 
+to get the list of our connection in secret
+'''
+
+class GetConnection():
+  
+  def __init__(self):
+    spark = SparkSession.builder.getOrCreate()
+    dbutils = DBUtils(spark)
+    self.scope = (
+                dbutils
+                .notebook
+                .entry_point
+                .getDbutils()
+                .notebook()
+                .getContext()
+                .tags()
+                .apply('user')
+                # XXX Hack incoming
+                .split('@')
+                [0]
+            )
+  # get secret
+  def get_secret(self, key):
+    spark = SparkSession.builder.getOrCreate()
+    dbutils = DBUtils(spark)
+    return dbutils.secrets.get(scope=self.scope, key=key)
+    
+  # providing list of creds
+  def get_connections(self):
+    list_cred = []
+    secret = self.get_secret('credentials.cfg')
+    txt = io.StringIO(secret)
+    parser = RawConfigParser()
+    config_parser = parser.read_file(txt)
+    # getting each url in credential file
+    for url in config_parser.sections():
+      path = URL(url)
+      # adding port
+      if((path.port == None) and (path.scheme != 's3a')):
+        # orracle
+        if(path.scheme == 'oracle'):
+          url_list = url.split('.com/')
+          url = url_list[0] + '.com' + ':1521' + '/' + url_list[1]
+        # redshift
+        elif(path.scheme == 'redshift'):
+          url_list = url.split('.com/')
+          url = url_list[0] + '.com' + ':5439' + '/' + url_list[1]
+      #appending list of url
+      list_cred.append(url)
+      
+    return list_cred
