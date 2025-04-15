52f4c5fe29068b2ca063b7275051c089955f4bbf
ziyan.zhu@thermofisher.com <ziyan.zhu@thermofisher.com>
2025-04-15 17:41:02 +0000

    separate databricks test

--- test/read_write_test.py
+++ test/read_write_test.py
@@ -69,30 +69,3 @@ class DataioReadWrite(NutterFixture):
 # excute_test from NutterFixture        
 result = DataioReadWrite().execute_tests()
 print(result.to_string())      
-
-# COMMAND ----------
-
-# MAGIC %load_ext autoreload
-# MAGIC %autoreload 2
-
-# COMMAND ----------
-
-from data_io import build_connection
-
-# COMMAND ----------
-
-urls_for_test = {
-  'redshift://user@rs-cdwdm-tst.c1rrn5bglols.us-east-1.redshift.amazonaws.com/rscdwdm': "lsgds.t_ds_bid_model",
-  'redshift://user@gitx-ops-data-warehouse.ccihmrvnkut2.us-east-1.redshift.amazonaws.com:5439/gitx': "lsg_cdp.profile",
-  'redshift://user@rs-edsr-prd.c7cbvhc6rtn1.us-east-1.redshift.amazonaws.com/edsr': "corpdm.vw_tmo_master",
-}
-
-# COMMAND ----------
-
-for url, tbl in urls_for_test.items():
-  connector = build_connection(url)
-  df = connector.read(
-        query=f"""SELECT
-                *
-              FROM {tbl} LIMIT 1""".strip()
-    )
