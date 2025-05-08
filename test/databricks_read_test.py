d4160eaabeb7d0faffb95411baac4365cf8326c8
ziyan-zhu-tfs <87032860+ziyan-zhu-tfs@users.noreply.github.com>
2025-05-08 00:07:15 -0400

    Remove URLs from public

--- test/databricks_read_test.py
+++ test/databricks_read_test.py
@@ -10,10 +10,6 @@ from urllib.parse import urlparse
 # COMMAND ----------
 
 urls_for_test = [
-  'redshift://user@rs-cdwdm-tst.c1rrn5bglols.us-east-1.redshift.amazonaws.com/rscdwdm.lsgds.t_ds_bid_model',
-  'redshift://user@rs-cdwdm-tst.c1rrn5bglols.us-east-1.redshift.amazonaws.com/rscdwdm.lsgmo.lsg_winback',
-  'redshift://user@gitx-ops-data-warehouse.ccihmrvnkut2.us-east-1.redshift.amazonaws.com:5439/gitx.lsg_cdp.profile',
-  'redshift://user@rs-edsr-prd.c7cbvhc6rtn1.us-east-1.redshift.amazonaws.com/edsr.corpdm.vw_tmo_master',
 ]
 
 # COMMAND ----------
