c2f05669c9d3f3ffb7d3d6d7c9c1291e30bf0cae
ziyan-zhu-tfs <87032860+ziyan-zhu-tfs@users.noreply.github.com>
2025-08-25 11:41:48 -0400

    Bump version from 1.0.0 to 1.0.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 setup(
     name='data_io',
     # Hard code for now
-    version='1.0.0',
+    version='1.0.1',
     description='Standardize data IO at Thermo Fisher',
     author='Christopher Bishop',
     author_email='chris.bishop@thermofisher.com',
